7fa65eda10c987bfd08f30fda4de0728722e4bbf
Fatah Ibrahim <ze5141be@go.minnstate.edu>
2025-10-31 11:50:00 -0500

    Staring the project

---
+++ BlackJack.py
@@ -0,0 +1,25 @@
+#Hello guys. What game are we doing?
+import random
+
+class Cards:
+    def __init__(self):
+
+        card_type = ['Spades', 'Clover', 'Heart', 'Diamonds']
+        self.card_type =  
+        self.card_num = card_num
+        '''[1,2,3,4,5,6,7,8,9,10]'''
+        self.special_cards = special_cards
+        '''['King', 'Queen', 'Jack', 'Ace'] '''
+
+    def get_card_type(self):
+        return self.card_type
+    def set_card_type(self,card):
+        self.card_type=card
+    
+    def get_card_num(self):
+        return self.card_num
+    
+    def get_special_cards(self)
+
+    def set_
+        
